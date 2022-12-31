a71f5c7e4f89fabe49e7e5678b998e0282deaeae
brennop <brennopcordeiro@gmail.com>
2022-12-31 19:10:23 -0300

    change pallet again

--- lib/gallery.ts
+++ lib/gallery.ts
@@ -1,18 +1,18 @@
 export default [
   "yi+t+C2+4^4+",
   "xh4-yh4-*t+S4*",
-  "ay-t4*y+Sx+S3*+D3+3<?5+", // flames from @2DArray
-  "x87*x8-D*y8-D*+-R/t4/S2++S2*f+", // moon
-  "ythS*xthC*+78*x8-D*y8-D*+-R/tS+S2*1-", // sphere
+  "ay-t4*y+Sxt++S3*+D2+2<?c+tS+", // flames from @2DArray
+  "2x87*x8-D*y8-D*+-R/t4/S2++S2*-", // moon
+  "ythS*xthC*+78*x8-D*y8-D*+-R/tS+S2*2+", // sphere
   "xta/+y5/S*t2/+C5*3&",
   "xt+y5/S*t+C3*",
   "xy+St+2/1+x+2%2+",
-  "ax2*Sxt2*+S-2y+-%8&2+S3+",
-  "x8-y4+/t2/+4*S2*1-",
+  "ax2*Sxt2*+S-2y+-%8&2+S4+",
+  "x8-y4+/t2/+4*S2*3+",
   "x8-y8-HtSt-+3%7+",
-  "xi^t3*+Cyi+t2*+C+^2+",
+  "xi^t3*+Cyi+t2*+C+0^6+",
   "2i*tS4/-t2/-Sl*f>",
-  "xyxy>?tS2*+t4*+S2*0^",
+  "xyxy>?tS2*+t4*+S2*4^",
   "ii*t4/+Sl*f>2+",
   "iyx*+t4/-Sl*f>4*",
   "iyx++t8/-Sl*S2*f^v3+",
