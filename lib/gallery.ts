9440055ff150246d5ff02e63c64273b4e0063371
brennop <brennopcordeiro@gmail.com>
2022-12-31 23:01:14 -0300

    palette update again

--- lib/gallery.ts
+++ lib/gallery.ts
@@ -7,11 +7,11 @@ export default [
   "xta/+y5/S*t2/+C5*3&",
   "xt+y5/S*t+C3*",
   "xy+St+2/1+x+2%2+",
-  "ax2*Sxt2*+S-2y+-%8&2+S4+",
-  "x8-y4+/t2/+4*S2*3+",
+  "ax2*Sxt2*+S-2y+-%8&2+Sb+", // waves
+  "x8-y4+/t2/+4*S2*2-", // sidewalk
   "x8-y8-HtSt-+3%7+",
-  "xi^t3*+Cyi+t2*+C+0^6+",
-  "2i*tS4/-t2/-Sl*f>",
+  "xi^t3*+Cyi+t2*+C+0^9+", // ocean
+  "2i*tS4/-t2/-Sl*f>9*1-", // rain
   "xyxy>?tS2*+t4*+S2*4^",
   "ii*t4/+Sl*f>2+",
   "iyx*+t4/-Sl*f>4*",
