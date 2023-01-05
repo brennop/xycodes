a3459d282e045b2850f42655c396ca6da703f281
brennop <brennopcordeiro@gmail.com>
2023-01-05 11:09:23 -0300

    add bloom

--- components/art.tsx
+++ components/art.tsx
@@ -7,6 +7,7 @@ import { draw } from "../lib/draw";
 
 export default function Art({ expression = "xy+", dynamic = true }) {
   const canvas = useRef<HTMLCanvasElement>(null);
+  const canvas2 = useRef<HTMLCanvasElement>(null);
 
   const [hovering, setHovering] = useState(false);
 
@@ -15,11 +16,14 @@ export default function Art({ expression = "xy+", dynamic = true }) {
   useEffect(() => {
     let frameId: number;
     const ctx = canvas.current?.getContext("2d");
+    const ctx2 = canvas2.current?.getContext("2d");
 
     function render(time: number) {
       if (ctx) draw(ctx, expression, time);
+      if (ctx2) ctx2.drawImage(canvas.current!, 0, 0);
 
       if (!dynamic && !hovering) return;
+
 
       frameId = requestAnimationFrame(render);
     }
@@ -40,7 +44,7 @@ export default function Art({ expression = "xy+", dynamic = true }) {
     >
       <div className="p-3 shadow-lg rounded-lg bg-white">
         <Link href={encodeURIComponent(expression)}>
-          <div className="relative mix-blend-hard-light contrast-50 opacity-25">
+          <div className="relative mix-blend-hard-light opacity-50 brightness-75">
             <svg viewBox="0 0 200 200" xmlns='http://www.w3.org/2000/svg' className="absolute">
               <filter id='noiseFilter'>
                 <feTurbulence
@@ -52,6 +56,9 @@ export default function Art({ expression = "xy+", dynamic = true }) {
 
               <rect width='100%' height='100%' filter='url(#noiseFilter)' />
             </svg>
+          </div>
+          <div className="relative overflow-none">
+            <canvas ref={canvas2} width={256} height={256} className="w-64 absolute blur-sm mix-blend-lighten opacity-50" />
           </div>
           <canvas ref={canvas} width={256} height={256} className="w-64" />
         </Link>
